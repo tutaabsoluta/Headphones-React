af00f51609ab2b8e574074c8ecb5154adfdf1979
Sergio Duran <538ser@gmail.com>
2024-10-08 12:44:31 -0600

    dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 
 export const Navbar = () => {
-
-  const [ isOpen, setIsOpen ] = useState( false );
+  const [isOpen, setIsOpen] = useState(false);
 
   const onToggle = () => {
-    setIsOpen( !isOpen );
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
-      <nav className="border-gray-200 dark:bg-gray-900 fixed top-0 left-0 w-full z-50">
+      <nav className="border-gray-700 bg-gray-900 fixed top-0 left-0 w-full z-50">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="#"
@@ -21,12 +20,12 @@ export const Navbar = () => {
             </span>
           </a>
           <button
-            onClick={ onToggle }
+            onClick={onToggle}
             data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600"
             aria-controls="navbar-default"
-            aria-expanded={ isOpen }
+            aria-expanded={isOpen}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -45,12 +44,12 @@ export const Navbar = () => {
               />
             </svg>
           </button>
-          <div className={`${ isOpen ? 'block' : 'hidden' } w-full md:block md:w-auto`}>
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`}>
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-700 rounded-lg bg-gray-800 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-900">
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
+                  className="block py-2 px-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-500"
                   aria-current="page"
                 >
                   Home
@@ -59,7 +58,7 @@ export const Navbar = () => {
               <li>
                 <a
                   href="#about"
-                  className="block py-2 px-3 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:border-0 md:hover:text-green-500"
                 >
                   About
                 </a>
@@ -67,7 +66,7 @@ export const Navbar = () => {
               <li>
                 <a
                   href="#services"
-                  className="block py-2 px-3 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:border-0 md:hover:text-green-500"
                 >
                   Services
                 </a>
@@ -75,7 +74,7 @@ export const Navbar = () => {
               <li>
                 <a
                   href="#pricing"
-                  className="block py-2 px-3 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:border-0 md:hover:text-green-500"
                 >
                   Pricing
                 </a>
@@ -83,7 +82,7 @@ export const Navbar = () => {
               <li>
                 <a
                   href="#contact"
-                  className="block py-2 px-3 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-white rounded hover:bg-green-500 md:hover:bg-transparent md:border-0 md:hover:text-green-500"
                 >
                   Contact
                 </a>
